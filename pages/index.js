d4ead4b5a8d76cddcbd0788152c3b8ca9212da0b
Thomas Kjellberg <thomaskj@hiqstom280.guest.stockholm.se>
2019-09-09 11:55:43 +0200

    Adding page nav

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,27 @@ export default class Home extends Component {
       attributes: { title, cats }
     } = content
     return (
-      <article>
-        <h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        <ul>
-          {cats.map((cat, k) => (
-            <li key={k}>
-              <h2>{cat.name}</h2>
-              <p>{cat.description}</p>
-            </li>
-          ))}
-        </ul>
-      </article>
+      <div>
+        <nav>
+          <ul>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+          </ul>
+        </nav>
+        <article>
+          <h1>{title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </div>
     )
   }
 }
